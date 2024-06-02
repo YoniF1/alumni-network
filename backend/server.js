f7a65e6ed31bfdfd8b91861b8102e7fab4819f9e
Yonatan <yonatanfilar@gmail.com>
2024-06-02 19:40:47 +0300

    Config to enable .env to be used & whitelist frontend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
-import dotenv from 'dotenv'
 import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
 import userrouter from '../backend/routes/users.r.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+))
+
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
