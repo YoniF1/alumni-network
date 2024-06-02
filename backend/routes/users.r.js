52c4730e91d96c1173f1f6687c33d9256d67eb04
Yonatan <yonatanfilar@gmail.com>
2024-06-02 19:42:20 +0300

    Add auth routes

--- backend/routes/users.r.js
+++ backend/routes/users.r.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { register, login, logout, refresh, all } from '../controllers/users.c.js'
+import { verifyToken } from '../middleware/verifyToken.js'
 
 const router = express.Router()
 
@@ -6,7 +8,19 @@ const router = express.Router()
 //     res.send("Hello")
 // })
 
-router.post('/register', )
+router.post('/register', register)
+router.post('/login', login)
+router.post('/logout', logout)
+
+router.get('/all', all)
+
+//to see if im authorised, redirect to page
+router.get("/verify", verifyToken, (req, res) => {
+    // create new token - refreshes the token - if you dont click then it'll redirect to login page
+    res.sendStatus(200)
+})
+
+router.post("/refresh", refresh)
+
 
 export default router
-
