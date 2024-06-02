be1f0a0a434ed164616dca2e7d533131f5badd8a
Yonatan <yonatanfilar@gmail.com>
2024-06-02 22:26:09 +0300

    Add navbar
    Uses material ui for navbar. Conditionally render of home & logout
    depending on if theres authenticated user.

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { Button, Stack, Toolbar, Typography, IconButton, AppBar, Box } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import { AuthContext } from '../App'
+import { AuthContextType } from '../types/consts'
+
+const Nav = () => {
+    const authContext = useContext(AuthContext) as AuthContextType;
+    const user = authContext?.user
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Alumni Network
+          </Typography>
+          <Stack direction='row' spacing={2}>
+            {user?.token ? <Button component={Link} to='/home' color="inherit">Home</Button> : null }
+            <Button component={Link} to='/login' color="inherit">Login</Button>
+            <Button component={Link} to='/register' color="inherit">Register</Button>
+            {user?.token ? <Button component={Link} to='/logout' color="inherit">Logout</Button> : null}
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Nav
