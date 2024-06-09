873c6329418910a5624716c0342e997a3e4ef9db
Yonatan <yonatanfilar@gmail.com>
2024-06-09 04:07:56 +0300

    Remove check to profile pic in redux store & use db call
    DB call is more reliable here as when user logs out redux store
    is reset so was causing an error

--- frontend/src/components/ProfilePicture.tsx
+++ frontend/src/components/ProfilePicture.tsx
@@ -6,20 +6,20 @@ import { Avatar } from "@mui/material"
 const ProfilePicture = () => {
   const { user } = useContext(AuthContext)
   const [profilePic, setProfilePic] = useState<string>('')
+  // const [biography, setBiography] = useState<string>('')
   
   const fetchProfilePic = async() => {
     const response = await axios.get(import.meta.env.VITE_BACKEND_URL + `/users/${user?.id}`)
-    console.log(response.data)
     setProfilePic(response.data.profile_picture)
   }
 
   useEffect(() => {
     fetchProfilePic()
-  }, [profilePic])  
+  }, [fetchProfilePic])  
 
-  return (
-    <Avatar alt="Profile Picture" src={profilePic} sx={{ width: 150, height: 150 }}/>
-  )
+  return profilePic && (
+    <Avatar alt="Profile Picture" src={profilePic} sx={{ width: 150, height: 150 }} />
+  );
 }
 
 export default ProfilePicture
