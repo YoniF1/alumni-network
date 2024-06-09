f8de54efc59a80bc766e18c28a457a6d9a9782ea
Yonatan <yonatanfilar@gmail.com>
2024-06-09 04:03:13 +0300

    Add controller actions, model functions and routes
    For the posts feature, endpoints are needed to fetch all cohort posts
    and to create posts for that cohort. Cohort name endpoint to present on posts component

--- backend/models/cohorts.m.js
+++ backend/models/cohorts.m.js
@@ -19,3 +19,39 @@ export const _unverifiedRequests = async() => {
         throw new Error("Could not retrieve requests")
     }
 }
+
+export const _cohortPosts = async(cohort_id) => {
+    try{
+        const cohortPosts = await db('post')
+                            .join('cohort', 'cohort.id', 'post.cohort_id')
+                            .join('users', 'users.id', 'post.user_id')
+                            .select('cohort.name', 'post.title', 'post.content', 'post.created_at', 'post.user_id', 'users.first_name', 'users.last_name')
+                            .where('post.cohort_id', '=', cohort_id)
+
+        return cohortPosts
+    } catch (error) {
+        console.log("Error in fetching cohort posts", error)
+        throw new Error("Could not get all the cohort posts")
+    }
+}
+
+export const _createCohortPost = async(cohort_id, title, content, user_id, created_at) => {
+    try {
+        const cohortPost = await db('post').insert({title, content, user_id, created_at, cohort_id}, ["id", "title", "content", "user_id", "created_at", "cohort_id"])
+        return cohortPost
+    } catch (error) {
+        console.log("Error in creating a post", error)
+        throw new Error("Could not create post")
+    }
+}
+
+
+export const _showCohortName = async(cohort_id) => {
+    try {
+        const cohortName = await db('cohort').select('name').where({id: cohort_id}).first()
+        return cohortName
+    } catch (error) {
+        console.log("Error in finding name of cohort", error)
+        throw new Error("Could not find name")
+    }
+}
